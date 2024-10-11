c2f104359831d1f0c902cb6f0553e592ad1cd70e
Reets123 <ibrahimalatig@gmail.com>
2024-10-11 02:06:49 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Lab 22: Upgrade the DLL Class
+// COMSC-210 - Ibrahim Alatig 
+
+#include <iostream>
+#include <cstdlib> 
+
+using namespace std;
+
+const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
+
+class DoublyLinkedList {
+private:
+    struct Node {
+        i
